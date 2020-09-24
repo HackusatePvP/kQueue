968e8da0880e25525da99ce5c4ffad0b02bafe8b
Hackusate_PvP <Supermftw@gmail.com>
2020-09-24 09:18:10 -0500

    Updated api see the wiki

--- src/main/java/cc/fatenetwork/kqueue/QueueAPI.java
+++ src/main/java/cc/fatenetwork/kqueue/QueueAPI.java
@@ -3,11 +3,16 @@ package cc.fatenetwork.kqueue;
 import cc.fatenetwork.kqueue.queue.Queue;
 import cc.fatenetwork.kqueue.queue.QueuePlayer;
 import cc.fatenetwork.kqueue.servers.Server;
+import org.bukkit.entity.Player;
 
 import java.util.Collection;
 import java.util.UUID;
 
 public class QueueAPI {
+
+    public QueueAPI() {
+        throw new UnsupportedOperationException("Cannot initiate util class");
+    }
 
     public static Queue getQueue(String name) {
         return Core.getPlugin().getQueueInterface().getQueue(name);
@@ -15,6 +20,34 @@ public class QueueAPI {
 
     public static boolean isQueue(String name) {
         return Core.getPlugin().getQueueInterface().isQueue(name);
+    }
+
+    public static void addPlayerToQueue(Player player, Queue queue) {
+        QueuePlayer queuePlayer = getQueuePlayer(player.getUniqueId());
+        if (queue != null && queuePlayer != null) {
+            Core.getPlugin().getQueueInterface().sendPlayerToServer(queuePlayer, queue);
+        }
+    }
+
+    public static void addPlayerToQueue(Player player, String queue) {
+        QueuePlayer queuePlayer = getQueuePlayer(player.getUniqueId());
+        if (isQueue(queue) && queuePlayer != null) {
+            Queue queueAct = getQueue(queue);
+            Core.getPlugin().getQueueInterface().sendPlayerToServer(queuePlayer, queueAct);
+        }
+    }
+
+    public static void addPlayerToQueue(QueuePlayer queuePlayer, Queue queue) {
+        if (queue != null && queuePlayer != null) {
+            Core.getPlugin().getQueueInterface().sendPlayerToServer(queuePlayer, queue);
+        }
+    }
+
+    public static void addPlayerToQueue(QueuePlayer queuePlayer, String queue) {
+        if (isQueue(queue) && queuePlayer != null) {
+            Queue queueAct = getQueue(queue);
+            Core.getPlugin().getQueueInterface().sendPlayerToServer(queuePlayer, queueAct);
+        }
     }
 
     public static Collection<Queue> getQueues() {
